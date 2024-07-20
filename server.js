d3b2c54bcf1008c1a4e105480980022466f7d843
Matheus Romeiro <matheuscontrabaixoc4@gmail.com>
2024-07-19 21:45:37 -0300

    update posts
    Funcionalidade das postagens finalizada com êxito!

--- server.js
+++ server.js
@@ -1,61 +1,56 @@
 import express from "express";
 import cors from "cors";
+import multer from "multer";
+import { storage } from "./multerConfig.js";
 import { PrismaClient } from "@prisma/client";
 
-/*
-    DB_USER: matheus
-    DB_PASSWORD: MFQklfbF31Rj7zpO
-*/
-
+const upload = multer({ storage: storage });
 const prisma = new PrismaClient();
-
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use("/uploads", express.static("uploads"));
 
-app.post("/posts", async (req, res) => {
-  await prisma.post.create({
+app.post("/posts", upload.single("file"), async (req, res) => {
+  const imagePath = req.file ? `/uploads/${req.file.filename}` : null;
+
+  const newPost = await prisma.post.create({
     data: {
       title: req.body.title,
       desc: req.body.desc,
-      image_url: req.body.image_url,
-      date_time: req.body.date_time,
+      image_url: imagePath,
+      date_time: new Date(req.body.date_time),
     },
   });
 
-  res.status(201).json(req.body);
+  res.status(201).json(newPost);
 });
 
 app.get("/posts", async (req, res) => {
   const posts = await prisma.post.findMany();
-
   res.status(200).json(posts);
 });
 
 app.put("/posts/:id", async (req, res) => {
-  await prisma.post.update({
-    where: {
-      id: req.params.id,
-    },
+  const updatedPost = await prisma.post.update({
+    where: { id: req.params.id },
     data: {
       title: req.body.title,
       desc: req.body.desc,
       image_url: req.body.image_url,
-      date_time: req.body.date_time,
+      date_time: new Date(req.body.date_time),
     },
   });
 
-  res.status(201).json(req.body);
+  res.status(201).json(updatedPost);
 });
 
 app.delete("/posts/:id", async (req, res) => {
-  await prisma.post.delete({
-    where: {
-      id: req.params.id,
-    },
-  });
-
+  await prisma.post.delete({ where: { id: req.params.id } });
   res.status(200).json({ message: "Usuário deletado com sucesso!" });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
